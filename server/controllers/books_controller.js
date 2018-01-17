6823770b6632a7e5c7e01733a4402e5dfb291bc1
Alan Curtis <talancurtis@gmail.com>
2018-01-17 09:48:55 -0700

    my code worked commented out example code

---
+++ server/controllers/books_controller.js
@@ -0,0 +1,70 @@
+var books = [];
+
+var id = 0
+
+module.exports = {
+    create:(req, res)=>{
+        const{title, author} = req.body;
+        books.push({id, title, author});
+        id++;
+        res.status(200).send( books );
+    },
+    read:(req, res)=>{
+        res.status(200).send (books);
+    },
+    update:(req, res)=>{
+        const updateId = req.params.id;
+        let index = books.findIndex( book => book.id == updateId);
+
+        books[ index ] = {
+            id: books[ index ].id,
+            title: req.body.title || books[index].title,
+            author: req.body.author || books[index].author
+        };
+        res.status(200).send(books);
+    },
+    delete:(req, res)=>{
+        const deleteId = req.params.id;
+        bookId = books.findIndex( book => book.id == deleteId);
+        books.splice( bookId, 1);
+        res.status(200).send(books);
+    }
+};
+
+
+// //////////////////////////////////
+// let books = [];
+// let id = 0;
+
+// module.exports = {
+//   create: ( req, res ) => {
+//     const { title, author } = req.body;
+//     books.push( { id, title, author } );
+//     id++;
+//     res.status(200).send( books );
+//   },
+
+//   read: ( req, res ) => {
+//     res.status(200).send( books );
+//   },
+
+//   update: ( req, res ) => {
+//     const updateID = req.params.id;
+//     let index = books.findIndex( book => book.id == updateID );
+
+//     books[ index ] = {
+//       id: books[ index ].id,
+//       title: req.body.title || books[ index ].title,
+//       author: req.body.author || books[ index ].author
+//     };
+
+//     res.status(200).send( books );
+//   },
+
+//   delete: ( req, res ) => {
+//     const deleteID = req.params.id;
+//     bookID = books.findIndex( book => book.id == deleteID );
+//     books.splice( bookID, 1 );
+//     res.status(200).send( books );
+//   }
+// };
